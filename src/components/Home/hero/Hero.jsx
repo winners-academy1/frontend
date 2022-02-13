c0c5f4f9b4a2d7f64b03e86c6ebf9a7091774f6c
Ahmad Abu Osbeh <ahmadabuosbeh20@gmail.com>
2022-02-13 21:25:07 +0200

    small fix

--- src/components/Home/hero/Hero.jsx
+++ src/components/Home/hero/Hero.jsx
@@ -1,28 +1,21 @@
-import React from "react";
-import "../../../styles/home-styles/hero.css";
-import { Carousel } from "react-bootstrap";
+import React from 'react';
+import '../../../styles/home-styles/hero.css';
+import { Carousel } from 'react-bootstrap';
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function Hero() {
-	return (
-		<>
-			<div className="hero">
-				<Carousel
-					className="carousel"
-					fade
-					controls={false}
-					indicators={false}
-					pause={false}
-				>
-					<Carousel.Item className="item1"></Carousel.Item>
+  return (
+    <>
+      <div className='hero'>
+        <Carousel className='carousel' fade controls={false} indicators={false} pause={false}>
+          <Carousel.Item className='item1'></Carousel.Item>
 
-					<Carousel.Item className="item2"></Carousel.Item>
-					<Carousel.Item className="item3"></Carousel.Item>
-				</Carousel>
-				<section className="lamar-logo">www.lamar-fashion.com</section>
-			</div>
-		</>
-	);
+          <Carousel.Item className='item2'></Carousel.Item>
+          <Carousel.Item className='item3'></Carousel.Item>
+        </Carousel>
+      </div>
+    </>
+  );
 }
 
 export default Hero;
